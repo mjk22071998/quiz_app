672dd5ea042def2e5a5976d37364b8a23cfbd368
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-17 17:35:19 +0500

    Layout ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,19 +32,54 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Quiz App"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Hello World',
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text("Quiz App"),
         ),
-      ),
-    );
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: PageView.builder(
+              itemBuilder: (context, index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Question: ${index + 1}/${questionsList.length}",
+                      style: const TextStyle(
+                          fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    Divider(
+                      height: 8,
+                      thickness: 2.0,
+                      color: Colors.grey.shade700,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: Text(
+                        questionsList[index].question,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    for (int i = 0;
+                        i < questionsList[index].answers.length;
+                        i++)
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            questionsList[index]
+                                .answers
+                                .entries
+                                .toList()[i]
+                                .key,
+                          ))
+                  ],
+                );
+              },
+              itemCount: questionsList.length,
+            ),
+          ),
+        ));
   }
 }
